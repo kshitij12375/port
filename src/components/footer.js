886542d007ba0abfd3120882135bb2d6bcaca52b
kshitij12375 <kshitijsrivastava505@gmail.com>
2025-01-30 20:37:39 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,59 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>&copy; 2025 Kshitij Kumar Srivastava. All rights reserved.</p>
+    <footer className="footer">
+      <div className="footer-container">
+        {/* Social Media Links */}
+        <div className="social-links">
+          <a
+            href="https://github.com/yourusername"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-icon"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a
+            href="https://linkedin.com/in/yourusername"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-icon"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a
+            href="https://twitter.com/yourusername"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-icon"
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </div>
+
+        {/* Copyright Text */}
+        <div className="copyright">
+          <p>&copy; 2023 Your Name. All rights reserved.</p>
+        </div>
+
+        {/* Back-to-Top Button */}
+        <div className="back-to-top">
+          <button
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            className="top-button"
+          >
+            ↑
+          </button>
+        </div>
+      </div>
     </footer>
   );
 };
